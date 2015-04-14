a225e36b25eb1bfe689a717e43858edca78cdb47
Sondsara <karp505@gmail.com>
2015-04-14 02:10:19 -0400

    Renamed

---
+++ v_scan/app/src/main/java/com/sondsara/v_scan/MainActivity.java
@@ -0,0 +1,160 @@
+package com.sondsara.v_scan;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+class animalIngredient implements Parcelable {
+    String name = "name";
+    String derivation = "derivation";
+    String status = "status";
+
+    public animalIngredient (Parcel source){
+        name = source.readString();
+        derivation = source.readString();
+        status = source.readString();
+    }
+
+    public animalIngredient (String name, String derivation, String status){
+        this.name = name;
+        this.derivation = derivation;
+        this.status = status;
+    }
+
+    public int describeContents(){
+        return this.hashCode();
+    }
+
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(name);
+        dest.writeString(derivation);
+        dest.writeString(status);
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
+        public animalIngredient createFromParcel (Parcel in){
+            return new animalIngredient(in);
+        }
+        public animalIngredient[] newArray(int size){
+            return new animalIngredient[size];
+        }
+    };
+}
+
+public class MainActivity extends ActionBarActivity {
+
+    public String upc = "";
+
+    private static final int ZBAR_SCANNER_REQUEST = 0;
+    private Cursor c;
+    private Database db;
+    public ArrayList<animalIngredient> animalProducts = new ArrayList<>();
+    public final static String EXTRA_MESSAGE = "com.Veganizer.main.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+        db= new Database(this);
+
+        try {
+            c = db.getIngredients();
+
+            while (c.moveToNext()) {
+                String name = c.getString(c.getColumnIndexOrThrow("NAME"));
+                String derivation = c.getString(c.getColumnIndexOrThrow("DERIVATION"));
+                String status = c.getString(c.getColumnIndexOrThrow("STATUS"));
+                animalIngredient temp = new animalIngredient(name, derivation, status);
+                animalProducts.add(temp);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        LaunchCamera();
+    }
+
+    void ScanButton() {
+        ImageButton start = (ImageButton) findViewById(R.id.imageButton);
+        start.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick (View vw) {
+                LaunchCamera();
+            }
+        });
+    }
+
+    void LaunchCamera(){
+        if (isCameraAvailable()){
+            Intent intent = new Intent (this, ZBarScannerActivity.class);
+            startActivityForResult (intent, ZBAR_SCANNER_REQUEST);
+        }else{
+            Toast.makeText(this, "YOU AIN'T GOT A CAMERA BOAH", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isCameraAvailable(){
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+        if (resultCode == RESULT_OK){
+            upc = data.getStringExtra(ZBarConstants.SCAN_RESULT);
+            if (upc.length() == 13) {
+                upc = upc.substring(1, 13);
+            }
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+            intent.putExtra(EXTRA_MESSAGE, upc);
+            intent.putParcelableArrayListExtra("key", animalProducts);
+            this.startActivity(intent);
+
+        }else if (resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "Camera Unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy (){
+        super.onDestroy();
+        c.close();
+        db.close();
+    }
+
+}
---
+++ v_scan/app/src/main/java/com/sondsara/v_scan/MainActivity2.java
@@ -0,0 +1,221 @@
+package com.sondsara.v_scan;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.factual.driver.Factual;
+import com.factual.driver.Query;
+import com.factual.driver.ReadResponse;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity2 extends Activity {
+
+    String upc = "";
+    protected Factual factual = new Factual("wDhW6eCPSv2BwdJvvQP63Pbpat7cWAkTnxSazIRM", "9ZIQc30A8DM0sX4qBtZoXE8DtZzf9X5sOQRR3bWf", true);
+    private animalIngredient[] badSeeds;
+    private ArrayList<animalIngredient> animalProducts;
+    private ListView list;
+    private Context context;
+    private ImageView pic;
+    private ImageView background;
+    private TextView resultText = null;
+    private ProgressBar progress;
+    private ImageButton reset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+
+        Intent intent = this.getIntent();
+        animalProducts = intent.getParcelableArrayListExtra("key");
+        upc = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        resultText = (TextView) findViewById(R.id.resultText);
+        list = (ListView) findViewById(R.id.listView);
+        context = this;
+        pic = (ImageView) findViewById(R.id.imageView);
+        background = (ImageView) findViewById(R.id.background);
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+        reset = (ImageButton) findViewById(R.id.restartButton);
+
+        background.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        FactualRetrievalTask task = new FactualRetrievalTask();
+        task.execute();
+
+        listenRestart();
+    }
+
+    void listenRestart() {
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View vw) {
+                restart();
+            }
+        });
+    }
+
+    void restart(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        this.startActivity(intent);
+        list.setVisibility(View.INVISIBLE);
+        pic.setVisibility(View.INVISIBLE);
+        reset.setVisibility(View.INVISIBLE);
+        progress.setVisibility(View.VISIBLE);
+        resultText.setVisibility(View.INVISIBLE);
+
+        ImageView window = (ImageView) findViewById(R.id.popupWindow);
+        TextView info = (TextView) findViewById(R.id.popupText);
+        TextView header = (TextView) findViewById(R.id.popupHeader);
+        if (window.getVisibility() == View.VISIBLE){
+            window.setVisibility(View.INVISIBLE);
+            info.setVisibility(View.INVISIBLE);
+            header.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected class FactualRetrievalTask extends AsyncTask<Query, Integer, ReadResponse> {
+
+        @Override
+        protected ReadResponse doInBackground (Query... params){
+            try {
+                Query query = new Query().field("upc").isEqual(upc);
+                ReadResponse resp = factual.fetch("products-cpg-nutrition", query);
+                return resp;
+            }catch (Exception e){
+                StringBuffer sb = new StringBuffer();
+                sb.append("I can't connect to the database.... Sorry!");
+                resultText.setText(sb.toString());
+                resultText.setVisibility(View.VISIBLE);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ReadResponse resp) {
+
+            StringBuffer sb = new StringBuffer();
+            sb.append("");
+            int isVegan = 1;
+            String contains = "";
+
+            if (resp != null) {
+
+                try {
+                    JSONArray ingredientsJSON = (JSONArray) resp.getData().get(0).get("ingredients");
+                    String[] ingredients = new String[ingredientsJSON.length()];
+                    badSeeds = new animalIngredient[ingredientsJSON.length()];
+
+                    for (int i = 0; i < ingredientsJSON.length(); i++) {
+
+                        if (!ingredientsJSON.getString(i).contains("Contains"))
+                            ingredients[i] = ingredientsJSON.getString(i);
+                        else
+                            contains = ingredientsJSON.getString(i);
+
+                        for (int j = 0; j < animalProducts.size(); j++) {
+                            if (ingredients[i].contains(animalProducts.get(j).name)) {
+                                if (animalProducts.get(j).name.equals(ingredients[i]))
+                                    badSeeds[i] = animalProducts.get(j);
+                                else {
+                                    String bsName = ingredients[i];
+                                    String bsDerivation = animalProducts.get(j).derivation;
+                                    String bsStatus = animalProducts.get(j).status;
+                                    animalIngredient temp = new animalIngredient(bsName, bsDerivation, bsStatus);
+                                    badSeeds[i] = temp;
+                                }
+                                if (animalProducts.get(j).status.equals("Not Vegan"))
+                                    isVegan = -1;
+                                else if (animalProducts.get(j).status.equals("Sometimes Vegan"))
+                                    isVegan = 0;
+                            } else if (contains.contains(animalProducts.get(j).name))
+                                if (animalProducts.get(j).status.equals("Not Vegan"))
+                                    isVegan = -1;
+                                else if (animalProducts.get(j).status.equals("Sometimes Vegan"))
+                                    isVegan = 0;
+                        }
+                    }
+
+                    if (isVegan == 1) {
+                        pic.setImageResource(R.drawable.vegan);
+                        background.setImageResource(R.drawable.green_background);
+                    } else if (isVegan == -1) {
+                        pic.setImageResource(R.drawable.not_vegan);
+                        background.setImageResource(R.drawable.red_background);
+                    } else if (isVegan == 0) {
+                        pic.setImageResource(R.drawable.maybe_vegan);
+                        background.setImageResource(R.drawable.orange_background);
+                    }
+
+                    ArrayList<HashMap<String, animalIngredient>> gris = new ArrayList<HashMap<String, animalIngredient>>();
+                    HashMap<String, animalIngredient> map;
+                    for (int q = 0; q < ingredients.length - 1; q+=2) {
+                        map = new HashMap<String, animalIngredient>();
+                        if (badSeeds[q] != null)
+                            map.put("one", badSeeds[q]);
+                        else {
+                            animalIngredient temp = new animalIngredient(ingredients[q], "", "");
+                            map.put("one", temp);
+                        }
+
+                        if (badSeeds[q + 1] != null)
+                            map.put("two", badSeeds[q + 1]);
+                        else {
+                            animalIngredient temp = new animalIngredient(ingredients[q + 1], "", "");
+                            map.put("two", temp);
+                        }
+                        gris.add(map);
+                    }
+
+                    CustomListAdapter adapt = new CustomListAdapter(context, gris);
+                    list.setAdapter(adapt);
+
+                    list.setVisibility(View.VISIBLE);
+                    pic.setVisibility(View.VISIBLE);
+                    reset.setVisibility(View.VISIBLE);
+                    progress.setVisibility(View.GONE);
+
+                } catch (Exception e) {
+                    sb.append("Sorry, I couldn't find that in the database. You might have to go this one alone.");
+                    resultText.setText(sb.toString());
+                    resultText.setVisibility(View.VISIBLE);
+                    e.printStackTrace();
+                }
+            }else{
+                sb.append("Sorry, it looks like the database isn't working right now... Try again later.");
+                resultText.setText(sb.toString());
+                resultText.setVisibility(View.VISIBLE);
+            }
+        }
+
+    }
+}
